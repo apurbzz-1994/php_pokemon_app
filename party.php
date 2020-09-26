2b359ece3c0ae1d4a12edc1801ffb91fda8a1d70
apurbzz-1994 <akash95august@gmail.com>
2020-09-27 04:24:30 +1000

    Add showing pokemon for each list in party page.

--- party.php
+++ party.php
@@ -61,7 +61,27 @@
                             </div>
                             <div class="col-12 col-md-8 col-lg-8">
                                 <!--Pokemon list goes here-->
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                                <?php 
+                                    $memberQuery = "SELECT pokemon.name AS name, pokemon.type_1 AS type, party_member.member_nickname AS nickname 
+                                    FROM pokemon JOIN party_member on (pokemon.id=party_member.pok_id AND py_id=?);";
+                                    $memberStmt = $dbh -> prepare($memberQuery);
+                                    $memberStmt -> execute([$party['py_id']]);
+                                    if($memberStmt -> rowCount() > 0){ 
+                                        $all_pokemon = $memberStmt -> fetchAll();?>
+                                    <?php foreach($all_pokemon as $pokemon){ ?>
+                                        <div>
+                                            <h4><?= $pokemon['name'] ?></h4>
+                                            <p><?= $pokemon['nickname'] ?></p>
+                                            <p><?= $pokemon['type'] ?></p>
+                                        </div>
+
+                                    <?php } ?>
+                                    <?php } else{ ?>
+                                        <!--Message if no pokemon belongs to that list-->  
+                                        <div>
+                                            <p>No pokemon has been added to the list as of now.</p>
+                                        </div>
+                                    <?php } ?>
                             </div>
                         </div>
                     </div>
